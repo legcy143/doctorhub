5533390997fdad2135a1637c09695c17070ec3a5
legcy143 <legcy143@gmail.com>
2022-06-03 19:57:40 -0400

    doctor hub
    doctor hub

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("hello ")
+
+let lis = document.getElementById("lis");
+// let close = document.getElementById("close");
+
+function open(){
+    lis.style.right = "00"
+    console.log("i am open")
+}
+function close(){
+   console.log("i am close")
+    lis.style.right = "-400"
+}
+
