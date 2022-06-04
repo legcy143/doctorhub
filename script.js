c1d71418eada7a1d9fe6682bfda932c3ea2ec9a6
legcy143 <96430904+legcy143@users.noreply.github.com>
2022-06-04 04:46:02 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-console.log("hello ")
 
-let lis = document.getElementById("lis");
-// let close = document.getElementById("close");
+let main = document.getElementById("main");
 
-function open(){
-    lis.style.right = "00"
-    console.log("i am open")
+function menuopen() {
+     main.style.right = "00";
 }
-function close(){
-   console.log("i am close")
-    lis.style.right = "-400"
+ function menuclose() {
+     main.style.right = "-460px";
 }
 
+
+// ==== mail function ===
+let gmail = document.getElementById("gmail")
+
+function sendEmail() {
+	Email.send({
+    SecureToken : "20ceb23f-bb69-40fc-80a0-f55273914e29 ",
+    To : 'legcy143@gmail.com',
+    From : "gamil.value",
+    Subject : "Doctur hub consultation",
+    // Body : "<html><h2>Header</h2><strong>Bold text</strong><br></br><em>Italic</em></html>"
+    }).then(
+        message => alert("message not send don't try again")
+    );
+}
+    // token   :     20ceb23f-bb69-40fc-80a0-f55273914e29 
